95d8df616a0a445dedf08f8b4b55777d94431d0f
alex <a.lemeshinsky@gmail.com>
2026-06-01 12:12:21 +0300

    fix(api): wire auth into axes module

--- apps/api/src/axes/axes.module.ts
+++ apps/api/src/axes/axes.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AxesController } from './axes.controller';
 import { AxesService } from './axes.service';
+import { AuthModule } from '../auth/auth.module';
 import { DatabaseModule } from '../database/database.module';
 import { QueuesModule } from '../queues/queues.module';
 
 @Module({
   controllers: [AxesController],
-  imports: [DatabaseModule, QueuesModule],
+  imports: [AuthModule, DatabaseModule, QueuesModule],
   providers: [AxesService],
   exports: [AxesService],
 })
